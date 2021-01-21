e97281f67f712fc15ad39f2536b9ff9b5b7c7f09
jinseon0328 <wishofsun@daum.net>
2021-01-21 19:21:35 +0900

    .

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -1,5 +1,10 @@
 package com.eomcs.pms;
 
+import com.eomcs.pms.handler.BoardHandler;
+import com.eomcs.pms.handler.BoardHandler2;
+import com.eomcs.pms.handler.BoardHandler3;
+import com.eomcs.pms.handler.BoardHandler4;
+import com.eomcs.pms.handler.BoardHandler5;
 import com.eomcs.pms.handler.MemberHandler;
 import com.eomcs.pms.handler.ProjectHandler;
 import com.eomcs.pms.handler.TaskHandler;
@@ -42,6 +47,29 @@ public class App {
           case "/task/list":
             TaskHandler.list();
             break;
+          case "/board/add":
+            BoardHandler.add();
+          case "/board/list":
+            BoardHandler.list();
+          case "/board2/add":
+            BoardHandler2.add();
+          case "/board2/list":
+            BoardHandler2.list();
+          case "/board3/add":
+            BoardHandler3.add();
+          case "/board3/list":
+            BoardHandler3.list();
+
+          case "/board4/add":
+            BoardHandler4.add();
+          case "/board4/list":
+            BoardHandler4.list();
+
+          case "/board5/add":
+            BoardHandler5.add();
+          case "/board5/list":
+            BoardHandler5.list();
+
           case "quit":
           case "exit":
             System.out.println("안녕!");
---
+++ app/src/main/java/com/eomcs/pms/handler/BoardHandler4.java
@@ -0,0 +1,45 @@
+package com.eomcs.pms.handler;
+import com.eomcs.pms.domain.Board;
+import com.eomcs.util.Prompt;
+
+public class BoardHandler4 {
+
+  static final int LENGTH = 100;
+  static Board [] Boards = new Board [LENGTH];  // 레퍼런스 배열 준비  
+  static int size = 0;
+
+  public static void add() {
+    System.out.println("[게시글 등록]");
+
+    // 1) 회원 정보를 담을 메모리를 준비한다.
+    Board b = new Board();
+
+    // 2) 사용자가 입력한 값을 Member 인스턴스에 저장한다.
+    b.no = Prompt.inputInt("번호? ");
+    b.title = Prompt.inputString("제목? ");
+    b.content = Prompt.inputString("내용? ");
+    b.writer = Prompt.inputString("작성자? ");
+    b.registeredDate = new java.sql.Date(System.currentTimeMillis());
+
+    System.out.println("게시글을 등록하였습니다");
+
+    // 3) 사용자의 정보가 저장된 인스턴스 주소를 레퍼런스 배열에 보관한다.
+    Boards [size++] = b;
+    // 위 문장은 컴파일할 때 다음 문장으로 변경된다.
+    //    int temp = size;
+    //    size++;
+    //    members[temp] = m;
+
+  }
+
+  public static void list() {
+    System.out.println("[게시글 목록]");
+
+    for (int i = 0; i < size; i++) {
+      Board b = Boards[i];
+      System.out.printf("%d, %s, %s, %s, %s, %d, %d\n", // 출력 형식 지정
+          b.no, b.title, b.content, b.writer, 
+          b.registeredDate, b.viewcount, b.like);
+    }
+  }
+}
